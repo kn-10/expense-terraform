6c6f785c7f31761bc514ef66be9b2f6675bf0552
metal <metalheadkrn@gmail.com>
2025-12-27 21:35:59 +0530

    Creating route entry to 2 rout tables of private subnet with internet gateway and also route table association along with peering connections

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -76,6 +76,7 @@ resource "aws_eip" "main" {
 }
 
 resource "aws_nat_gateway" "main" {
+  count          = length(var.public_subnets_cidr)
   allocation_id = aws_eip.main.id
   subnet_id     = lookup(element(aws_subnet.public, count.index), "id", null)
 
