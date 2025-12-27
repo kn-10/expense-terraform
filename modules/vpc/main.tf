3d40f8ce453e281e1bf5c873b0dcaf443817e353
metal <metalheadkrn@gmail.com>
2025-12-27 20:59:47 +0530

    Creating route entry to 2 rout tables of public subnet with internet gateway and also route table association along with peering connections

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -13,6 +13,17 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_vpc_peering_connection" "main" {
+
+  vpc_id        = aws_vpc.main.id
+  peer_vpc_id   = data.aws_vpc.default.id
+  auto_accept = true
+  tags = {
+    Name = "${var.env}-vpc-with-default-vpc"
+  }
+
+}
+
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
@@ -41,6 +52,11 @@ resource "aws_route_table" "public" {
     gateway_id = aws_internet_gateway.main.id
   }
 
+  route {
+    cidr_block                = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+
 
 
   tags = {
@@ -48,10 +64,15 @@ resource "aws_route_table" "public" {
   }
 }
 
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets_cidr)
+  route_table_id = lookup(element(aws_route_table.public, count.index),"id", null) #aws_route_table.public[count.index].id
+  subnet_id = lookup(element(aws_subnet.public,count.index),"id", null)
+}
 resource "aws_subnet" "private" {
-  count = length(var.private_subnets_cidr)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.private_subnets_cidr,count.index)
+  count             = length(var.private_subnets_cidr)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnets_cidr,count.index)
   availability_zone = element(var.az,count.index)
 
   tags = {
@@ -59,28 +80,19 @@ resource "aws_subnet" "private" {
   }
 }
 
-resource "aws_vpc_peering_connection" "main" {
 
-  vpc_id        = aws_vpc.main.id
-  peer_vpc_id   = data.aws_vpc.default.id
-  auto_accept = true
-  tags = {
-    Name = "${var.env}-vpc-with-default-vpc"
-  }
-
-}
 
 resource "aws_route" "main" {
 
-  route_table_id = aws_vpc.main.main_route_table_id
-  destination_cidr_block = data.aws_vpc.default.cidr_block
+  route_table_id            = aws_vpc.main.main_route_table_id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
 }
 
 resource "aws_route" "default-vpc" {
 
-  route_table_id = data.aws_vpc.default.main_route_table_id
-  destination_cidr_block = aws_vpc.main.cidr_block
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = aws_vpc.main.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
 }
 
@@ -122,9 +134,9 @@ resource "aws_security_group" "test" {
 }
 
 resource "aws_instance" "test" {
-  ami = data.aws_ami.example.image_id
-  instance_type = "t3.micro"
-  subnet_id = aws_subnet.private[0].id
+  ami                    = data.aws_ami.example.image_id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.private[0].id
   vpc_security_group_ids = [aws_security_group.test.id]
 }
 
