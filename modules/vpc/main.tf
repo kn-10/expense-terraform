33624bb287c0e68b454b549926c1f4f098c2af71
metal <metalheadkrn@gmail.com>
2025-12-27 19:58:02 +0530

    Creating internet gateway to be attached to VPC

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -13,6 +13,14 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-${var.project_name}-igw"
+  }
+}
+
 resource "aws_subnet" "public" {
   count = length(var.public_subnets_cidr)
   vpc_id     = aws_vpc.main.id
