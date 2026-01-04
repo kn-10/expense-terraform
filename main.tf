9b017a756bfec5ac829c0261c34a77043e617031
metal <metalheadkrn@gmail.com>
2026-01-04 17:49:25 +0530

    Since public load balancer is in picture ,  sg_cidr_blocks of frontend will be in public subnets cidr

--- main.tf
+++ main.tf
@@ -92,7 +92,7 @@ module "frontend" {
   instance_capacity = var.frontend_instance_capacity
   instance_type = var.frontend_instance_type
   project_name = var.project_name
-  sg_cidr_blocks = var.web_subnets_cidr  # This needs to be checked
+  sg_cidr_blocks = var.public_subnets_cidr # This needs to be checked
   vpc_id = module.vpc.vpc_id
   vpc_zone_identifier = module.vpc.web_subnets_id
 }
