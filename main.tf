144c0e9a4749c43394747c9a78c4a1b64297bdf7
metal <metalheadkrn@gmail.com>
2026-01-04 17:48:08 +0530

    Since private load balancer is in picture ,  sg_cidr_blocks of backend will be in app subnets cidr

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ module "backend" {
   instance_capacity = var.backend_instance_capacity
   instance_type = var.backend_instance_type
   project_name = var.project_name
-  sg_cidr_blocks = var.web_subnets_cidr  # This needs to be checked
+  sg_cidr_blocks = var.app_subnets_cidr # This needs to be checked
   vpc_id = module.vpc.vpc_id
   vpc_zone_identifier = module.vpc.app_subnets_id
 }
