5186854d176ff52fc159bc1b14a72db3df5cdc4e
metal <metalheadkrn@gmail.com>
2026-01-07 15:36:15 +0530

    Making changes for expense.conf and creating userdata.sh for launch templates with ansible pull mechanism

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -3,5 +3,5 @@
 sudo dnf install ansible python3.12-pip.noarch -y &>> /opt/userdata.log
 sudo pip3.12 install boto3 botocore &>> /opt/userdata.log
 
-ansible-pull -i localhost, -U https://github.com/kn-10/expense-ansible.git expense.yml -D -e service_name=frontend -e env=dev
+ansible-pull -i localhost, -U https://github.com/kn-10/expense-ansible.git expense.yml -e service_name=frontend -e env=dev
 
