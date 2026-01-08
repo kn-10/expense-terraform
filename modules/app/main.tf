9a208553b17aeea3ad91306d3af4ed6aec612f9c
metal <metalheadkrn@gmail.com>
2026-01-08 16:17:52 +0530

    Module backend depends on module rds as mysql should be running before and so we are defining dependency here

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -127,7 +127,7 @@ resource "aws_iam_role" "main" {
           ],
           "Resource": concat([
             "arn:aws:kms:us-east-1:367241114876:key/b0eaa327-c037-47e6-93ed-78b8b08219b9",
-            "arn:aws:ssm:us-east-1:367241114876:parameter/${var.env}.${var.project_name}.${var.component}.*"
+            "arn:aws:ssm:us-east-1:367241114876:parameter/${var.env}.${var.project_name}.${var.component}.*",
           ], var.parameters)
         },
         {
